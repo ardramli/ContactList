cca17a61b6a85020290abdd369032de0e845b0a3
ardMac <adliramli88@gmail.com>
2017-05-22 23:47:09 +0800

    first commit

--- ContactList/ViewController.m
+++ ContactList/ViewController.m
@@ -8,22 +8,70 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController () <UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIButton *buttonAdd;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong,nonatomic) NSMutableArray *contacts;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self prepareContacts];
+    [self prepareTableView];
+    [self prepareButtons];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)prepareTableView {
+    //[self.tableView setDataSource:self]; another way to do the below code
+    self.tableView.dataSource = self; //set the datasource
+    
 }
+
+-(void)prepareContacts {
+    self.contacts = [@[@"kim",@"rock",@"naruto"] mutableCopy]; //mutableCopy means i will create a mutable copy for this array, because array literals are not compatible with mutable array
+    
+}
+
+-(void)prepareButtons {
+    [self.buttonAdd addTarget:self action:@selector(addNewContacts) forControlEvents:UIControlEventTouchUpInside];
+    
+}
+
+-(void)addNewContacts {
+    NSString *contactName = self.textField.text;
+    [self.contacts addObject:contactName];
+    [self.tableView reloadData]; //to refresh table whenever you add new data into table
+    //not the most efficient
+    //but reloadData will re render everything. imagine if you have millions of data
+    //also can do it like below
+    //    self.tableView insertRowsAtIndexPaths:<#(nonnull NSArray<NSIndexPath *> *)#> withRowAnimation:<#(UITableViewRowAnimation)#>
+    
+}
+
+
+
+#pragma mark - UITableView DataSource
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.contacts.count; //this will determine number of rows
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"contactCell" forIndexPath:indexPath];
+    
+    [cell.textLabel setText:self.contacts[indexPath.row]];
+    
+    
+    return cell;
+}
+
 
 
 @end
